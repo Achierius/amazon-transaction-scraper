b44a30b43b5e9f86287b1a612b68c500d1190519
Marcus Plutowski <achierius@gmail.com>
2025-03-20 11:40:48 -0700

    Allow user to filter transactions by date
    Also fixes a bug where we just look at the first page again and again.

--- amazon-order-scraper/controller.py
+++ amazon-order-scraper/controller.py
@@ -2,6 +2,7 @@ from rich.progress import Progress
 
 from .scrape_order_list import load_order_page_and_get_order_count_for_year, scrape_order_summaries
 from .scrape_invoice import parse_invoice
+from .config import OrderFilter
 
 def collect_order_summaries(driver, year : str, order_count : int):
     all_summaries = []
@@ -10,6 +11,7 @@ def collect_order_summaries(driver, year : str, order_count : int):
                                        total=order_count)
         while len(all_summaries) < order_count:
             target_page = f"https://www.amazon.com/your-orders/orders?timeFilter=year-{year}&startIndex={len(all_summaries)}"
+            # TODO Logging print(target_page)
             summaries = scrape_order_summaries(driver, target_page)
             progress.update(status_bar, advance=len(summaries))
             all_summaries.extend(summaries)
@@ -17,15 +19,14 @@ def collect_order_summaries(driver, year : str, order_count : int):
     return all_summaries
 
 
-def filter_order_summaries(summaries):
-    # TODO filter by date
-    return summaries
+def filter_order_summaries(summaries, filter : OrderFilter):
+    return [s for s in summaries if (s.order_date <= filter.end_date and s.order_date >= filter.start_date)]
 
 
 def scrape_invoices(driver, order_summaries):
     parsed_orders = []
     with Progress() as progress:
-        status_bar = progress.add_task("[blue]Scraping Amazon transactions...",
+        status_bar = progress.add_task(f"[blue]Processing {len(order_summaries)} Amazon invoices...",
                                        total=len(order_summaries))
 
         for summary in order_summaries:
@@ -35,10 +36,13 @@ def scrape_invoices(driver, order_summaries):
     return parsed_orders
 
 
-def parse_amazon_transactions_for_year(driver, year : str):
+def parse_amazon_transactions_for_year(driver, filter : OrderFilter):
     # Start on the generic per-year page to see how many orders there are
-    order_count = load_order_page_and_get_order_count_for_year(driver, year)
-    order_summaries = collect_order_summaries(driver, year, order_count)
-    order_summaries = filter_order_summaries(order_summaries)
+    order_count = load_order_page_and_get_order_count_for_year(driver, filter.year)
+    order_summaries = collect_order_summaries(driver, filter.year, order_count)
+    order_summaries = filter_order_summaries(order_summaries, filter)
+    if not order_summaries:
+        print(f"No orders found in given date-range: {filter.start_date} to {filter.end_date}")
+        return []
     orders = scrape_invoices(driver, order_summaries)
     return orders
