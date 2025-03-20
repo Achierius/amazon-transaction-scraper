927df15a70b164be6c154b5bd99a8e6df66a8d0b
Marcus Plutowski <achierius@gmail.com>
2025-03-20 13:14:19 -0700

    Correctly parse invoices with multiple shipping dates

--- amazon-order-scraper/datatypes.py
+++ amazon-order-scraper/datatypes.py
@@ -10,17 +10,18 @@ class Item:
     description: str
     sold_by: str
     supplied_by: str
+    shipping_date: Optional[datetime]
 
     def __str__(self):
         return (f"{self.count}x '{self.description}' @ ${self.unit_price:.2f} each\n"
                 f"  Sold by: {self.sold_by}\n"
-                f"  Supplied by: {self.supplied_by if self.supplied_by else 'N/A'}")
+                f"  Supplied by: {self.supplied_by if self.supplied_by else 'N/A'}\n"
+                f"  Shipping Date: {self.shipping_date.strftime('%Y-%m-%d') if self.shipping_date else 'N/A'}")
 
 
 @dataclass
 class Order:
     order_date: datetime
-    shipping_date: datetime
     order_number: str
     total: float
     sub_total: float
@@ -32,10 +33,9 @@ class Order:
             f"Order #{self.order_number}\n"
             f"Link: <{self.url}>\n"
             f"Order Date: {self.order_date.strftime('%Y-%m-%d')}\n"
-            f"Shipping Date: {self.shipping_date.strftime('%Y-%m-%d') if self.shipping_date else 'N/A'}\n"
             f"Order Total: ${self.total:.2f}\n"
             f"Sub-Total: ${self.sub_total:.2f}\n"
-            f"Items:\n"
+            f"Items:"
         )
         for item in self.items:
             order_str += f"  {str(item)}\n"
@@ -57,6 +57,6 @@ class OrderSummary:
             f"Invoice Link: <{self.invoice_url}>\n"
             f"Order Date: {self.order_date.strftime('%Y-%m-%d')}\n"
             f"Order Total: ${self.total:.2f}\n"
-            f"Status: {delivered_str}\n"
+            f"Status: {delivered_str}"
         )
 
